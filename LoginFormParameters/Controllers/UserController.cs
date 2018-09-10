07d5c877f94497b9bffe1584322e5cf31323cb36
manideep.n15 <manideep.n15@gmail.com>
2018-09-10 12:20:41 +0530

    commited

---
+++ LoginFormParameters/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginFormParameters.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LoginFormParameters.Controllers
+{
+    public class UserController : Controller
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User u, string UserName, string Password)
+        {
+           UserName = u.UserName;
+          Password = u.Password;
+            if (ModelState.IsValid)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select UserName,Password from UserInfo where UserName=@name and Password=@password", con);
+                cmd.Parameters.AddWithValue("@name", UserName);
+                cmd.Parameters.AddWithValue("@password", Password);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                if (dt.Rows.Count > 0)
+                {
+                    return View("DashBoard");
+                }
+                else
+                {
+                    ViewBag.error = "Invalid UserName or Password";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+
+        }
+    }
+}
---
+++ LoginFormParameters/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginFormParameters.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "enter username")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "enter password")]
+        public string Password { get; set; }
+    }
+}
